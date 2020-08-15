e9d817329ba3bd7f88556993acc7b598ef97901c
sajad saderi <saderi.sajad@gmail.com>
2020-08-15 17:47:06 +0430

    test

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const recaptcha = new Recaptcha(siteKey, secretKey, {action: 'homepage'});
 
 app.get('/recaptcha/', recaptcha.middleware.verify, (req, res) => {
   const botCookie = req.cookies._rbs;
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
     res.setHeader('Access-Control-Allow-Credentials', true);
